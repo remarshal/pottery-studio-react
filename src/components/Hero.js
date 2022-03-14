8252d878f73a1cffa54377a3e9060d41f356627a
remarshal <marshalwaldendev@gmail.com>
2022-03-14 14:53:40 -0400

    added bootstrap css.min

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,21 @@
 import "./Hero.css";
+import { Container, Row, Col } from "reactstrap";
 import hero from "../images/studio.png";
 
 function Hero() {
     return (
         <header>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
+            <Container>
+                <Row>
+                    <Col>
                         <h1 className="hero--header">Next Restock</h1>
                         <p className="hero--text">Join our mailing list and never miss shop restocks!</p>
-                    </div>
-                    <div className="col-lg-6">
+                    </Col>
+                    <Col>
                         <img src={hero} className="hero--photo"></img>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                </Row>
+            </Container>
         </header>
     );
 }
