a9bba9b34b579819c4cd21c491e63c0a69bb41c1
remarshal <marshalwaldendev@gmail.com>
2022-03-14 16:42:06 -0400

    removed typed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,7 @@ import hero from "../images/studio.png";
 function Hero() {
     
     return (
-        const typed = new Typed('#typed',{
-            stringsElement: '#typed-strings',
-            backSpeed: 100,
-            typeSpeed: 100,
-            loop: true,
-        })
+
             
         <header>
             <div className="container">
