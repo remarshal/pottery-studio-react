47627a7b66e4693fc2c806d67b766a78bd51f8c3
remarshal <marshalwaldendev@gmail.com>
2022-03-14 15:26:47 -0400

    container max-width

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,20 @@
 import "./Hero.css";
-import { Container, Row, Col } from "reactstrap";
 import hero from "../images/studio.png";
 
 function Hero() {
     return (
         <header>
-            <Container>
-                <Row>
-                    <Col>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
                         <h1 className="hero--header">Next Restock</h1>
                         <p className="hero--text">Join our mailing list and never miss shop restocks!</p>
-                    </Col>
-                    <Col>
-                        <img src={hero} className="hero--photo"></img>
-                    </Col>
-                </Row>
-            </Container>
+                    </div>
+                    <div className="col-lg-6">
+                        <img src={hero} className="hero--photo img-fluid rounded-3 my-5"></img>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
