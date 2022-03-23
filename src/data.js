dff920df05380053beef2bae7738cd61f157cfa8
remarshal <marshalwaldendev@gmail.com>
2022-03-23 12:39:12 -0400

    data.js

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 1,
+        img: "mug-1.png",
+        title: "Nantahala",
+        price: 105.00,
+        details: "4.25 in tall by 4 in wide",
+        rating: 5
+    },
+    {
+        id: 1,
+        img: "mug-2.png",
+        title: "Luray",
+        price: 95.00,
+        details: "4.25 in tall by 4 in wide",
+        rating: 5
+    },
+    {
+        id: 1,
+        img: "mug-3.png",
+        title: "Damascus",
+        price: 120.00,
+        details: "4.25 in tall by 4 in wide",
+        rating: 5
+    },
+    {
+        id: 1,
+        img: "mug-4.png",
+        title: "Franklin",
+        price: 115.00,
+        details: "4.25 in tall by 4 in wide",
+        rating: 5
+    },
+]
